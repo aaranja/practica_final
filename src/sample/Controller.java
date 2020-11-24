7e9bf6eb6fc872cb03b3db400b9cc4f288aa128a
aaranja <fidxle@gmail.com>
2020-11-23 18:46:14 -0800

    - Modificación de la interfaz.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,8 +4,10 @@ import javafx.embed.swing.SwingNode;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+
 import javafx.scene.control.TextArea;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.FileChooser;
 import javax.swing.*;
@@ -16,16 +18,23 @@ import sample.analizador.CargarGramatica;
 
 public class Controller {
     @FXML private TextArea txtentrada;
+    @FXML private TextArea txt_salida;
     @FXML private Pane output_pane;
     @FXML private ChoiceBox<String> choice_grammar;
+    @FXML private Menu menu_grammar;
     private final SwingNode swingnode2 = new SwingNode();
     File seleccionado;
     String nombre_gramatica="carbohidratos.Calorias"; // default
     JTextArea txtConsole = new JTextArea(200,40);
     String folder_path = null;
 
-    public void analizar (MouseEvent evento) throws IOException {
+    public void analizar (ActionEvent evento) throws IOException {
         txtConsole.setText("");
+
+        // Selección de la gramática
+        MenuItem sub_menu = (MenuItem) evento.getSource();
+        nombre_gramatica = sub_menu.getText();
+
         String path_input="t.expr";
         String frase=txtentrada.getText();
         FileWriter escribir= new FileWriter(path_input, StandardCharsets.UTF_8);
@@ -34,26 +43,20 @@ public class Controller {
         escribir.close();
 
         // Ejecutar la gramática indicada
-        nombre_gramatica = choice_grammar.getValue();
         CargarGramatica gramatica = new CargarGramatica();
         String salida_grammar = gramatica.invocarClase(nombre_gramatica, "analizar", path_input);
         if(salida_grammar != null){
             System.out.println(salida_grammar);
 
             // Crear panel de salida
-            JPanel output = new JPanel();
-            output.setBounds(0,0,100,100);
-            txtConsole.setText(salida_grammar);
-            output.add(txtConsole);
+
+            txt_salida.setText(salida_grammar);
 
             // Verifica si el panel de javaFx esta vacío, remueve su contenido
-            if (!output_pane.getChildren().isEmpty()){
-                output_pane.getChildren().remove(swingnode2);
-            }
+
 
             // Carga el contenido en el panel de JavaFX
-            swingnode2.setContent(output);
-            output_pane.getChildren().add(swingnode2);
+
         }
     }
 
@@ -84,15 +87,16 @@ public class Controller {
            FileChooser fc = new FileChooser();
            fc.setInitialDirectory(new File(folder_path));
 
-           if(!txtConsole.getText().equals("")){
+           if(!txt_salida.getText().equals("")){
                switch (nombre_gramatica) {
                    case "carbohidratos.Calorias":
                        FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("Archivos (*.txt)","*.txt");
                        fc.getExtensionFilters().add(filter);
                        File seleccionad = fc.showSaveDialog(null);
+
                        if (seleccionad!=null)
                        {
-                           String salida = txtConsole.getText();
+                           String salida = txt_salida.getText();
                            FileWriter escritura  = new FileWriter(seleccionad);
                            for(int i=0;i<salida.length();i++)
                            {
@@ -105,8 +109,9 @@ public class Controller {
                        FileChooser.ExtensionFilter filter2 = new FileChooser.ExtensionFilter("Archivos (*.csv)","*.csv");
                        fc.getExtensionFilters().add(filter2);
                        File seleccionado = fc.showSaveDialog(null);
+                       System.out.println(txt_salida.getText());
                        if(seleccionado!=null) {
-                           String salida = txtConsole.getText();
+                           String salida = txt_salida.getText();
                            FileWriter escritura = new FileWriter(seleccionado);
 
                            escritura.write("Numero");
@@ -116,12 +121,11 @@ public class Controller {
                            escritura.write("Promedio \n");
 
                            for (String fila : salida.split("\\n")) {
-
                                int sad = fila.lastIndexOf(" ");
                                String promedio = ",".concat(fila.substring(sad + 1));
                                String nombre = fila.substring(0, sad);
                                nombre = nombre.replace("] ", "],");
-                               String resultado = nombre.concat(promedio);
+                               String resultado = nombre.concat(promedio.concat("\n"));
                                escritura.write(resultado);
                            }
                            escritura.close();
@@ -182,8 +186,9 @@ public class Controller {
 
     }
 
-    public void limpiarEntrada(MouseEvent evento){
+    public void limpiarEntrada(ActionEvent evento){
         txtentrada.setText("");
+        txt_salida.setText("");
     }
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.lang.*;
 
 public class Main extends Application {
@@ -14,7 +13,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Proyecto integrador");
-        primaryStage.setResizable(false);
+        primaryStage.setResizable(true);
         primaryStage.setScene(new Scene(root, 1200, 600));
         primaryStage.show();
     }
